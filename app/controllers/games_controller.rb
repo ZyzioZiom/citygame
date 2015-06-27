44e3eedec56df40072441371e6a51f087e369155
Patryk Kalinowski <patryk.kalinowski@gmail.com>
2015-06-27 21:39:08 +0200

    Implemented game results

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -12,8 +12,29 @@ class GamesController < ApplicationController
     render json: data
   end
   
-  def send_result
-    # send result 
+  def send_time
+    game = Game.find(params[:game_id])
+    
+    # zakładamy że drugi gracz się nie broni i atakujący wygrywa
+    game.update(attacker_time: params[:attacker_time], defender_time: nil, winner: self.attacker_id)
+    
+    data = {
+      code: 200
+      winner: game.winner
+      }
+    
+    render json: data
+  end
+  
+  def request_result(game_id)
+    # send result to player after defender finished his turn and players request result
+    game = Game.find(game_id)
+    
+    data = {
+      winner: game.winner
+      }
+    
+    render json: data
   end
   
 end
