58676b2625cb1db3b213c5dd26c5a8a5e5d5c7c2
Patryk Kalinowski <patryk.kalinowski@gmail.com>
2015-06-27 15:08:23 +0200

    New user fields

--- db/migrate/20150627130348_add_fields_to_user.rb
+++ db/migrate/20150627130348_add_fields_to_user.rb
@@ -1,4 +1,9 @@
 class AddFieldsToUser < ActiveRecord::Migration
   def change
+    add_column :users, :beacon_id, :string
+    add_column :users, :firstname, :string
+    add_column :users, :lastname, :string
+    add_column :users, :avatar_name, :string
+    add_column :users, :avatar_description, :string
   end
 end
