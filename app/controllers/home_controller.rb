d48b071136e77907b06bffb4e0163ffc4e9e0a7b
Patryk Kalinowski <patryk.kalinowski@gmail.com>
2015-06-28 02:21:03 +0200

    Home back to normal

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 #  before_action :authenticate_user!
   
   def index
-#    @games = Game.order(created_at: :desc)
+    @games = Game.order(created_at: :desc)
   end
   
   def last_games
